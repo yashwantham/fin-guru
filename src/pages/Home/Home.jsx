136c1ae3d70363c4e318e7a7ba995058c1a8d3a4
Yashwanth A M <yashwanthyashu166@gmail.com>
2024-05-26 16:35:29 +0530

    no result found fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -37,8 +37,16 @@ function Home() {
     const searchResult = detailResponse.find(
       ({ Prompt, Response }) => searchedTerm === Prompt
     );
-    console.log("searchResult", searchResult);
-    setSelectedResponse(() => searchResult);
+    if (searchResult !== undefined) {
+      console.log("searchResult", searchResult);
+      setSelectedResponse(() => searchResult);
+    } else {
+      setSelectedResponse(() => ({
+        Prompt: "How Do I Save More Money?",
+        Response:
+          "A budget can help you save money, but many people won't use one if it feels difficult, dull or restrictive. Here's how to\nmake budgeting and saving easy and empowering.\nFirst, play detective by tracking your income and expenses for 30 days—an app can save time here. Next, use the\nresults to set up a budget that fits your goals and your daily routine. For example, if your month of tracking reveals\nyou're spending more than you want on dining out, set a monthly ceiling. Then, before you order takeout, check your\nspending so you stay on budget. You don't have to completely revamp your spending in one month. Use your tracking data to identify changes that feel\neasy, and implement them one at a time. This way, you'll boost your savings balance without feeling overwhelmed or\ndeprived. Adding a \"cheat day\" category to your budget can also give you a little wiggle room for expenses during\nthose extra-hard day.Accelerate your progress by bucketing—automatically depositing money in separate accounts, each designated\nfor a key goal. You'll save time and stay on track because once you've set up the automatic deposit, you don't have to\ndo a thing.",
+      }));
+    }
   }
 
   return (
